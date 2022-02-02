232966c6ea652b640e9f1215f249e29bda700404
maxchai123 <62576022+maxchai123@users.noreply.github.com>
2022-02-01 16:14:00 -0800

    Add files via upload

--- dice_simulation.py
+++ dice_simulation.py
@@ -5,6 +5,16 @@ class Dice:
     
     def roll(self):
         return random.randint(1,6)
+class Bank:
+    def __init__(self, balance, owner_name):
+        self.balance = balance
+        self.owner_name = owner_name
+    
+    def withdraw(self, my_money):
+        self.balance -= my_money
+
+    def deposit(self, new_money):
+        self.balance += new_money
 
 def greet():
     print("====================================")
@@ -14,6 +24,7 @@ def greet():
 def main():
 
     # Initialize variables and die
+    my_bank_account = Bank(1000, "Max Chai")
     sum_of_outcomes = 0
     die = Dice()
 
@@ -23,11 +34,20 @@ def main():
     desired_number_of_rolls = int(input("How many times would you like to roll the die? "))
     
     for i in range(desired_number_of_rolls):
+        outcome = die.roll()
         sum_of_outcomes += die.roll()
+        my_bank_account.withdraw(3.6)
+        my_bank_account.deposit(outcome)
+
 
     # Results
     average = round(sum_of_outcomes / desired_number_of_rolls, 1)
+    my_bank_account_balance = my_bank_account.balance
+    net_proceeds = my_bank_account_balance - 1000
     print("The average number was: {}".format(str(average)))
+    print()
+    print("The average won/lost is: {}".format(round(net_proceeds / desired_number_of_rolls, 2)))
+
     
 
 main()
